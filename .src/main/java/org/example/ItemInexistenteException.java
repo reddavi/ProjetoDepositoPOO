4b7f8214913322439dee0987633cc9cc509f0e7f
reddavi <juba1999eutu@hotmail.com>
2024-05-04 23:16:57 -0300

    Upando Enum

--- .src/main/java/org/example/ItemInexistenteException.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class ItemInexistenteException extends Exception {
-    public ItemInexistenteException(String msg) {
-        super(msg);
-    }
-}
--- .src/main/java/org/example/tipoEstoqueCategoria.java
+++
@@ -1,5 +0,0 @@
-package org.example;
-
-public enum tipoEstoqueCategoria {
-    PESADO,LEVE
-}
